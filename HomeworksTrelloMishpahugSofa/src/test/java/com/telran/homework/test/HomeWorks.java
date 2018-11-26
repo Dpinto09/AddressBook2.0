355ef3eacce23d12160d7666d5d39977745075fe
Dpinto09 <42576010+Dpinto09@users.noreply.github.com>
2018-11-26 15:05:57 +0200

    finish

--- HomeworksTrelloMishpahugSofa/src/test/java/com/telran/homework/test/HomeWorks.java
+++ HomeworksTrelloMishpahugSofa/src/test/java/com/telran/homework/test/HomeWorks.java
@@ -1,13 +1,11 @@
 package com.telran.homework.test;
 
 import com.telran.homework.model.LoginData;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.util.List;
 
 public class HomeWorks extends TestBase {
@@ -65,40 +63,76 @@ public class HomeWorks extends TestBase {
         buttonHomeLeftSideMenu();
         wait3000();
         fillNewBoardFormWithoutTeam("DragAndDropNote");
-        fillNewBoardFormWithTeam("DragAndDropNote");
+        //fillNewBoardFormWithTeam("DragAndDropNote");
         addList("List1");
         wait3000();
         addNewList("List2");
         wait3000();
         addCardInFirstList("Card1");
         wait3000();
-        addNewCardInFirstList("Card2");
+        addAnotherCardInFirstList("Card2");
+        wait3000();
+        addNewCardInSecondList("Card3");
+        wait3000();
+        addAnotherCardButtonInSecondList("Card4");
 
-        List<WebElement> cardsList1 = wd.findElements(By.xpath("//div[1]//div[1]//div[1]//div[1]//div[1]//div[1]//div[@class='list-cards u-fancy-scrollbar u-clearfix js-list-cards js-sortable ui-sortable']//a"));
-        for (int i = 1; i <= cardsList1.size(); i++) {
+        WebElement List1 = wd.findElement(By.xpath("//div[@class='js-list list-wrapper'][1]"));
 
-            WebElement cards = wd.findElement(By.xpath("//div[1]//div[1]//div[1]//div[1]//div[1]//div[1]//div[@class='list-cards u-fancy-scrollbar u-clearfix js-list-cards js-sortable ui-sortable']//a[" + i + "]"));
-            System.out.println(cards.getText());
+        List<WebElement> cardsList1 = List1.findElements(By.cssSelector(".list-card"));
+
+        for(WebElement cardTitle: cardsList1){
+
+            System.out.println(cardTitle.getText());
         }
-        WebElement firstCard = wd.findElement(By.xpath("//a[@style='position: relative; z-index: 0; left: 0px; top: 0px;']"));
-        WebElement list2 = wd.findElement(By.xpath("//div[@id='board']/div[2]/div[1]/a[1]"));
-        new Actions(wd).dragAndDrop(firstCard,list2).release().perform();
 
+        WebElement firstCard = wd.findElement(By.xpath("//div[@id='board']/div[1]/div[1]/div[2]/a[1]"));
+        WebElement list2 = wd.findElement(By.xpath("//div[@id='board']//div[2]//div[1]//div[@class='list-cards u-fancy-scrollbar u-clearfix js-list-cards js-sortable ui-sortable']"));
+        new Actions(wd).dragAndDrop(firstCard, list2).release().perform();
 
+        WebElement List2 = wd.findElement(By.xpath("//div[@class='js-list list-wrapper'][2]"));
+        List<WebElement> cardsList2 = List2.findElements(By.cssSelector(".list-card"));
+        for(WebElement cardTitle: cardsList2){
+
+            System.out.println(cardTitle.getText());
+        }
     }
 
-    public void addNewCardInFirstList(String cardTitle) {
-        click(By.xpath("//div[@id='board']//div[1]//div[1]//a[1]//span[@class='js-add-another-card']"));
+    public void addNewCardInSecondList(String cardTitle) {
+
+        addCardButton();
         type(By.xpath("//textarea[@placeholder='Enter a title for this card…']"), cardTitle);
         click(By.xpath("//input[@value='Add Card']"));
         click(By.xpath("//a[@class='icon-lg icon-close dark-hover js-cancel']"));
+    }
+
+    public void addAnotherCardInFirstList(String cardTitle) {
+        addAnotherCardButtonInFirstList();
+        type(By.xpath("//textarea[@placeholder='Enter a title for this card…']"), cardTitle);
+        click(By.xpath("//input[@value='Add Card']"));
+        click(By.xpath("//a[@class='icon-lg icon-close dark-hover js-cancel']"));
+    }
+
+    public void addAnotherCardButtonInSecondList(String cardTitle) {
+
+        click(By.xpath("//div[@id='board']//div[2]//div[1]//a[1]//span[@class='js-add-another-card']"));
+        type(By.xpath("//textarea[@placeholder='Enter a title for this card…']"), cardTitle);
+        click(By.xpath("//input[@value='Add Card']"));
+        click(By.xpath("//a[@class='icon-lg icon-close dark-hover js-cancel']"));
+    }
+
+    public void addAnotherCardButtonInFirstList() {
+        click(By.xpath("//div[@id='board']//div[1]//div[1]//a[1]//span[@class='js-add-another-card']"));
     }
 
     public void addCardInFirstList(String cardTitle) {
-        click(By.xpath("//div[@id='board']//div[1]//div[1]//a[1]//span[2]"));
+        addCardButton();
         type(By.xpath("//textarea[@placeholder='Enter a title for this card…']"), cardTitle);
         click(By.xpath("//input[@value='Add Card']"));
         click(By.xpath("//a[@class='icon-lg icon-close dark-hover js-cancel']"));
+    }
+
+    public void addCardButton() {
+        click(By.xpath("//span[@class='js-add-a-card']"));
     }
 
 
